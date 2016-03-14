1914f59bfd6245710a5df1ef8431a95ae05aab4b
Mark Madsen <mark@madsenlab.org>
2016-03-14 08:26:54 -0700

    added options to confusion_heatmap for transparency, reverse color scheme for dark slide presentations, and automatically saving the graphic to a file.

--- sklearn_mmadsen/_version.py
+++ sklearn_mmadsen/_version.py
@@ -1,4 +1,4 @@
 
 # This file is updated from Git information by running 'python setup.py
 # version'.
-__version__ = '0aa221'
+__version__ = 'deea18'
--- sklearn_mmadsen/graphics/__init__.py
+++ sklearn_mmadsen/graphics/__init__.py
@@ -1,7 +1,55 @@
 import seaborn as sns
 
-def confusion_heatmap(y_test, y_pred, labels):
+def confusion_heatmap(y_test, y_pred, class_labels = None, axis_labels = None, title = None, transparent = False, reverse_color = False, filename = None):
+    """
+    Draw a Seaborn heatmap from a sklearn confusion matrix, with optional
+    text labels.  Returns the matplotlib AX object for use later, or saving
+    to a file.
+
+    Optional axis labels and title parameters are available; axis labels have reasonable defaults if not given, and
+    no title is shown by default.
+
+    For use in slide presentations, there is an option for making the graphic background transparent when saving it
+    to a file, and to reverse the color scheme of axis elements to white for placing the graphic on a dark background.
+
+    :param y_test:
+    :param y_pred:
+    :param class_labels: optional
+    :param axis_labels: optional
+    :param title: optional
+    :param transparent: optional
+    :param reverse_color: optional
+    :param filename: optional
+    :return:
+    """
     from sklearn.metrics import confusion_matrix
+
+    if reverse_color is True:
+        custom_style = {'axes.labelcolor': 'white',
+                'xtick.color': 'white',
+                'ytick.color': 'white'}
+        sns.set_style("darkgrid", rc=custom_style)
+
+
     mat = confusion_matrix(y_test, y_pred)
     ax = sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
-                     xticklabels=labels, yticklabels=labels)
+                     xticklabels=class_labels, yticklabels=class_labels)
+
+    if axis_labels is None:
+        xlabel = "Predicted Class"
+        ylabel = "Actual Class"
+    else:
+        xlabel = axis_labels[0]
+        ylabel = axis_labels[1]
+
+    if title is not None:
+        ax.set_title(title)
+
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+
+    if filename is not None:
+        fig = ax.get_figure()
+        fig.savefig(filename, dpi=300, transparent=transparent)
+
+    return ax
